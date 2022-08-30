677f4c7bf48bd086f8aa580ea2d5b280dd459b5f
greatt8696 <https://github.com/greatt8696>
2022-08-30 18:39:57 +0900

    jwt 생성

--- view/index.js
+++ view/index.js
@@ -5,10 +5,10 @@ $.getJSON("/getDatas", (datas) => {
   dbManager.createTableEl(table, ["balance", "uid", "grade"]);
 });
 
-// $.getJSON("/getDatas2", (datas) => {
-//   dbManager.setData(datas);
-//   dbManager.createTableEl(table2);
-// });
+$.getJSON("/getDatas2", (datas) => {
+  dbManager.setData(datas);
+  dbManager.createTableEl(table2);
+});
 
 // $.getJSON("/getDatas3", (datas) => {
 //   dbManager.setData(datas);
