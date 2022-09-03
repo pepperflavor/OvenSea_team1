ac7b8f5d0ca86f278d457cd81fb11f9c4a37def4
greatt8696 <greatt8696@gmail.com>
2022-09-03 11:52:40 +0900

    axios_util db 세팅

---
+++ util/axios_util.js
@@ -0,0 +1,63 @@
+const { send } = require("express/lib/response");
+
+const makeParams = (data) => {
+  const dataObj = { ...data };
+  const params = new URLSearchParams();
+  const headers = {
+    "Content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+    Accept: "*/*",
+  };
+
+  for (const [key, data] of Object.entries(dataObj)) {
+    params.append(key, data);
+  }
+
+  return { params, headers };
+};
+
+/**
+ *
+ * @param {Object} objData
+ * @return {PromiseLike}
+ */
+const sendAxios = async (objData) => {
+  const { url, params, headers } = objData;
+  return axios.post(url, params, { headers });
+};
+
+class ApiHandler {}
+
+//if (e.target.value !== "") {
+//  const params = new URLSearchParams();
+//  const headers = {
+//    "Content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+//    Accept: "*/*",
+//  };
+//  params.append("user_email", e.target.value);
+//  axios
+//    .post("/existEmail", params, {
+//      headers,
+//    })
+//    .then(function (response) {
+//      const {
+//        data: { msg },
+//      } = response;
+//     emailExist.innerHTML = msg;
+//    });
+//} else {
+//  e.target.value = "";
+//}
+
+const testObj = {
+  uid: "admin1",
+  pwd: "$2b$10$3q1d0sraTQ6OUOuCXP4yjOqHdZBiSqiciyIwXHbAQksu956Hio/zS",
+  name: "admin1",
+  email: "admin@naver.com",
+  balance: 987654321098765,
+  grade: 2,
+  state: 0,
+};
+
+const { params, headers } = makeParams(testObj);
+
+console.log(params);
