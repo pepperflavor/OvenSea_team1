7e188ba41d234f6cb24677842c9386c49df44173
greatt8696 <https://github.com/greatt8696>
2022-09-07 12:38:54 +0900

    aws 서버용

--- config/config.js
+++ config/config.js
@@ -9,6 +9,13 @@ const config = {
     host: "127.0.0.1", // 여기에 만약 우리가 AWS RDS 쓰거나 지원 데이터베이스 등등 사용을 한다면 이곳에 주소를 넣으면 됩니다.
     dialect: "mysql", // dial
   },
+  prod: {
+    username: "admin",
+    password: process.env.DATABASE_PASSWORD,
+    database: "ovensea",
+    host: "127.0.0.1", // 여기에 만약 우리가 AWS RDS 쓰거나 지원 데이터베이스 등등 사용을 한다면 이곳에 주소를 넣으면 됩니다.
+    dialect: "mysql", // dial
+  },
 };
 
 const session_config = {
