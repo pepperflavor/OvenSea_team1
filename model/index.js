13bc11f6326ce03888359bb8f2a3a35add01490f
greatt8696 <greatt8696@gmail.com>
2022-09-08 07:02:56 +0900

    db

--- model/index.js
+++ model/index.js
@@ -13,9 +13,9 @@ const Room = require("./rooms")
 
 // console.log(config);
 
-const { database, username, password } = config.dev;
+const { database, username, password } = config.prod;
 
-const sequelize = new Sequelize(database, username, password, config.dev);
+const sequelize = new Sequelize(database, username, password, config.prod);
 
 // 내보내기 위한 빈객체
 const db = {};
